e8e497803b10023f13099ec0555c071a867c40d2
Joachim Bakke <github@heiamoss.com>
2017-01-18 23:36:02 +0100

    Sending commands to Seatalk over NMEA0183

--- index.js
+++ index.js
@@ -8,19 +8,8 @@ const react = require('react/package.json') // react is a peer dependency.
 const rjf = require('react-jsonschema-form')
 
 // Seatalk1:
-/*const lampOff = "80,00,00"
-const lampdim = "80,00,04"
-const lampOn = "80,00,08"
-const lampBright = "80,00,0C"*/
 const seaTalk = ["80,00,00", "80,00,04", "80,00,08", "80,00,0C"]
 /*80  00  0X      Set Lamp Intensity: X=0 off, X=4: 1, X=8: 2, X=C: 3*/
-
-const lightLevel = [
-  "off",
-  "dim",
-  "on",
-  "bright"
-]
 
 var refresh
 var altitude
@@ -147,7 +136,66 @@ function isItTime (app, props){
     debug("Sending command " + lightLevel + " to instruments")
     if (props.Seatalk1){
       seatalkCommand = seaTalk[lightLevel]
-      debug(seatalkCommand)
+
+      debug("seatalkCommand: " + seatalkCommand)
+      nmea0183out = toSentence([
+        '$STALK',
+        seatalkCommand
+      ]);
+
+      debug("nmea0183out: " + nmea0183out)
+      app.emit('nmea0183out', nmea0183out)
     }
   }, the_interval)
+}
+
+function toSentence(parts) {
+  var base = parts.join(',')
+  return base + computeChecksum(base)
+}
+var m_hex = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F'
+]
+
+function computeChecksum(sentence) {
+  var c1
+  var i
+
+  // skip the $
+  i = 1
+
+  // init to first character    var count;
+
+  c1 = sentence.charCodeAt(i)
+
+  // process rest of characters, zero delimited
+  for (i = 2; i < sentence.length; ++i) {
+    c1 = c1 ^ sentence.charCodeAt(i)
+  }
+
+  return '*' + toHexString(c1)
+}
+
+function toHexString(v) {
+  var lsn
+  var msn
+
+  msn = (v >> 4) & 0x0f
+  lsn = (v >> 0) & 0x0f
+  return m_hex[msn] + m_hex[lsn]
 }
